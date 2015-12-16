fd98cf52db037d83ca23564e5bc02b2133d38e88
Jake Blakeley <jakeblakeley@fb.com>
2015-12-16 12:51:42 -0800

    removed all tests

--- js/main.js
+++ js/main.js
@@ -47,7 +47,6 @@ $(document).ready(function(){
 			//clean up language titles
 			if (language !== null && language !== ""){
 				var sortLanguage = language.toLowerCase();
-				console.log(sortLanguage);
 				if (sortLanguage === "javascript"){
 					language = "JS";
 				} else if (sortLanguage === "objective-c"){
